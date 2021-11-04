adc666611ca230fcd07408954ff6a59c55af59cf
monieshravichandrran <monieshravichandrran@gmail.com>
2021-11-04 20:15:14 +0530

    refer

--- client/src/App.js
+++ client/src/App.js
@@ -37,21 +37,21 @@ const App = () => {
     <>
       <Router history={history}>
         <Route path='/' exact component={LoginPage} />
-        {state.type == '0' ??<Route path="/admin" exact component={Admin}/>}
-        {state.type == '1' ??<Route path='/user' exact component={User} />}
-        {state.type == '2' ??<Route path='/insurance' exact component={Insurance} />}
-        {state.type == '3' ?? <Route path='/hospital' exact component={Hospital} /> }
+        {type == '0' ??<Route path="/admin" exact component={Admin}/>}
+        {type == '1' ??<Route path='/user' exact component={User} />}
+        {type == '2' ??<Route path='/insurance' exact component={Insurance} />}
+        {type == '3' ?? <Route path='/hospital' exact component={Hospital} /> }
         <Route path="/forgotpassword" exact component={forgotPassword} />
         <Route path="/signup" exact component={Signup} />
         <Route path="/chat" exact component={Chat} />
-        {state.type == '2'??<Route path="/insurance/grant" exact component={MoneyGrant} />}
-        {state.type == '1' ?? <Route path="/user/accept" exact component={Userinsaccept} />}
-        {state.type == '2' ?? <Route path="/insurance/profile" exact component={insProfile} />}
-        {state.type == '3' ?? <Route path="/hospital/profile" exact component={hospProfile} />}
-        {state.type == '2' ?? <Route path="/insurance/transactions" exact component={insTrans} />}
-        {state.type == '3' ?? <Route path="/hospital/transactions" exact component={hospTrans} />}
-        {state.type == '3' ?? <Route path="/hospital/bill" exact component={HosIns} />}
-        {state.type == '2' ?? <Route path="/insurance/bill" exact path="/insurance/bill" component={InsuranceBill} />}
+        {type == '2'??<Route path="/insurance/grant" exact component={MoneyGrant} />}
+        {type == '1' ?? <Route path="/user/accept" exact component={Userinsaccept} />}
+        {type == '2' ?? <Route path="/insurance/profile" exact component={insProfile} />}
+        {type == '3' ?? <Route path="/hospital/profile" exact component={hospProfile} />}
+        {type == '2' ?? <Route path="/insurance/transactions" exact component={insTrans} />}
+        {type == '3' ?? <Route path="/hospital/transactions" exact component={hospTrans} />}
+        {type == '3' ?? <Route path="/hospital/bill" exact component={HosIns} />}
+        {type == '2' ?? <Route path="/insurance/bill" exact path="/insurance/bill" component={InsuranceBill} />}
       </Router>
     </>
   );
