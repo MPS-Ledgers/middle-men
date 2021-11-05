93b72cbbe4af7e4bb24728c8f8b6672873d039b6
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2021-11-05 10:07:50 +0530

    fixed package

--- client/src/pages/chat.js
+++ client/src/pages/chat.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { ChatEngine } from 'react-chat-engine';
-import './chat.css'
-const Chat=()=> {
-    return (
-        <ChatEngine
-            height='100vh'
-            userName='abc@x.com'
-            userSecret='123456'
-            projectID='3a749869-1abb-4bd5-8a4a-9d34ba95e983'
-        />
-    );
-}
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./chat.css";
+const Chat = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      userName="abc@x.com"
+      userSecret="123456"
+      projectID="3a749869-1abb-4bd5-8a4a-9d34ba95e983"
+    />
+  );
+};
 
-export default Chat
+export default Chat;
