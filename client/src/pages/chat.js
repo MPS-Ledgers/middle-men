618fa6100c86d4a37f49865139ca05024a53693f
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2021-11-05 10:23:31 +0530

    added dynamic user change to chat

--- client/src/pages/chat.js
+++ client/src/pages/chat.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
+import { useSelector } from "react-redux";
 import "./chat.css";
 const Chat = () => {
+  const auth = useSelector((state) => state.auth);
   return (
     <ChatEngine
       height="100vh"
-      userName="abc@x.com"
+      userName={auth.user.email}
       userSecret="123456"
       projectID="3a749869-1abb-4bd5-8a4a-9d34ba95e983"
     />
